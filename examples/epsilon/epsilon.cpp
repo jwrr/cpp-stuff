411a0a1af44e04466eb207f0e882df23a23f7f51
jwrr <jwrr.com@gmail.com>
2023-02-03 00:56:24 -0500

    example/epsilon - add template

--- examples/epsilon/epsilon.cpp
+++ examples/epsilon/epsilon.cpp
@@ -1,36 +1,42 @@
 // epsilon.cpp
 // Measure smallest value that makes a difference for float, double and long 
 // double.
+// Reference: https://www.youtube.com/playlist?list=PLzn6LN6WhlN1Lic9M9mbzqV3ASokOdnFM
+// Course: Programming Techniques for Scientific Simulation 1
+// Lecturer: Matthias Troyer
 
 #include <iostream>
 
 using namespace std;
 
+template <typename T>
+T epsilon()
+{
+  T one = 1.0;  
+  T eps = one;
+  while (true) {
+    // cout << "T eps = " << eps << endl;
+    T eps_next = eps / 2.0;
+    if (one == one + eps_next) {
+      break;
+    }
+    eps = eps_next;
+  }
+  return eps;
+}
 
 int main()
 {
   cout << "hello\n";
 
-  float f_one = 1.0;  
-  float f_eps = f_one;
-  while (f_one != f_one + f_eps) {
-    cout << "float f_eps = " << f_eps << endl;
-    f_eps /= 2.0;
-  }
+  float f_eps = epsilon<float>();
+  cout << "float eps = " << f_eps << endl;
 
-  double d_one = 1.0;  
-  double d_eps = d_one;
-  while (d_one != d_one + d_eps) {
-    cout << "double d_eps = " << d_eps << endl;
-    d_eps /= 2.0;
-  }
-  
-  long double ld_one = 1.0;
-  long double ld_eps = ld_one;
-  while (ld_one != ld_one + ld_eps) {
-    cout << "long double ld_eps = " << ld_eps << endl;
-    ld_eps /= 2.0;
-  }
-  
+  double d_eps = epsilon<double>();
+  cout << "double eps = " << d_eps << endl;
+
+  long double ld_eps = epsilon<long double>();
+  cout << "long double eps = " << ld_eps << endl;
+
   return 0;
 }
